7d3b6ac970ec87046f91522e6c9263621ecfaadc
FabricioLuisdeSousaSantos <fabricioluis100@gmail.com>
2025-07-31 10:03:05 -0300

    adição do ORM e criação das vars de ambiente 02

--- BancoDeDados.py
+++ BancoDeDados.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine, text, MetaData, select, Table
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 db_host     = os.getenv("DB_HOST")
 db_port     = os.getenv("DB_PORT")
 db_name     = os.getenv("DB_NAME")
